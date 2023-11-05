50a3ef0a323b10df983a264764f780fa25049089
Ezra Natanael <ezranatanael2003@gmail.com>
2023-11-05 14:12:21 +0700

    feat: added main function

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function sumAll(arr) {
+    arr.sort((a, b) => a - b);
+
+    let sum = 0;
+
+    for (let i = arr[0]; i <= arr[1]; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+module.exports = sumAll;
